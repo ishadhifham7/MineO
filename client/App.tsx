92b450bffd2098ba6c20f402451ef06b1465c148
Ishadh Ifham <ishadhifham@gmail.com>
2026-02-01 11:48:18 +0530

    frontend fix

--- client/App.tsx
+++ client/App.tsx
@@ -5,7 +5,7 @@ import "./src/styles/global.css";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+      <Text className="text-xl font-bold text-green-500">
         Welcome to Nativewind!
       </Text>
       <StatusBar style="auto" />
