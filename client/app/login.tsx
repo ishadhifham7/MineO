b29ed409feeb01d9936ee656c2018327fae8cbd8
mohommedMalik <mohommed.abu.malik@gmail.com>
2026-02-19 02:48:12 +0530

    Deleted some fils for modification of UIs

--- client/app/login.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import LoginScreen from "./auth/AuthScreens/LoginScreen";
-
-export default function Login() {
-  return <LoginScreen />;
-}
--- client/app/signup.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import SignupScreen from "./auth/AuthScreens/SignupScreen";
-
-export default function Signup() {
-  return <SignupScreen />;
-}
