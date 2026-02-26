d1bf7714af6bf5b200af404a2326187e8546188d
cheerysaark <arulalagna.20232033@gmail.com>
2026-02-26 11:30:15 +0530

    layer commit

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -2,7 +2,6 @@ import "../src/styles/global.css";
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function TabsLayout() {
   return (
